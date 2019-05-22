2648aa133c751ea5e4d904d23a6585b41bea14a2
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2019-05-22 15:49:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# About SExtractor++ 
+
+[![Build Status](https://travis-ci.org/astrorama/conda-sextractorxx.svg?branch=master)](https://travis-ci.org/astrorama/conda-sextractorxx)
+
+Home: https://github.com/astrorama/sextractorxx.git 
+
+Package license: LGPL 3.0
+
+Summary: The next generation SExtractor. 
+
---
+++ recipe/sextractorxx_launch.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+if [[ -f /usr/bin/osascript ]]; then
+    /usr/bin/osascript -e 'tell application "Terminal" to activate' \
+        -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down' \
+        -e 'tell application "Terminal" to do script "sextractor++ --version" in selected tab of the front window'
+fi
+
