bd1a3280b8c749744c3a20ab637bd58a40175e90
Alejandro Álvarez Ayllón <a.alvarezayllon@gmail.com>
2019-05-23 09:26:52 +0200

    Activate environment before running sextractor++

--- recipe/sextractorxx_launch.sh
+++ recipe/sextractorxx_launch.sh
@@ -3,6 +3,7 @@
 if [[ -f /usr/bin/osascript ]]; then
     /usr/bin/osascript -e 'tell application "Terminal" to activate' \
         -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down' \
+        -e 'tell application "Terminal" to do script "conda activate '${CONDA_DEFAULT_ENV}'" in selected tab of the front window' \
         -e 'tell application "Terminal" to do script "sextractor++ --version" in selected tab of the front window'
 fi
 
