1aff29e9a40c6e05bf0846164a879d6e776f75b3
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2020-12-22 05:24:33 -0800

    Build on arm64-apple-darwin

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,7 @@ COMPILER_ID=$(echo ${COMPILER_VERSION} | awk 'BEGIN {FS=".";} { printf $1$2 }')
 
 declare -a CMAKE_PLATFORM_FLAGS
 if [[ -n "${OSX_ARCH}" ]]; then
-    CMAKE_PLATFORM_FLAGS+=(-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}")
+    CMAKE_PLATFORM_FLAGS+=(-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}" -DCLANG_C_COMPILER="$(which ${CC})" -DCLANG_CXX_COMPILER="$(which ${CXX})")
     PLATFORM="darwin"
     COMPILER=llvm
 else
@@ -13,6 +13,7 @@ else
     PLATFORM="linux"
     COMPILER=gcc
 fi
+ARCH=$(echo $HOST | cut -d- -f1)
 
 BUILD_DIR="${SRC_DIR}/build"
 mkdir -p "${BUILD_DIR}"
@@ -23,7 +24,8 @@ export CMAKE_PROJECT_PATH="${PREFIX}/lib/cmake/ElementsProject:${PREFIX}/lib64/c
 # Elements will auto-detect macports, and we do not want that
 export MACPORT_LOCATION=/tmp/xxxx
 
-export BINARY_TAG="x86_64-${PLATFORM}-${COMPILER}${COMPILER_ID}-opt"
+export BINARY_TAG="${ARCH}-${PLATFORM}-${COMPILER}${COMPILER_ID}-opt"
+export VERBOSE=1
 
 cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
     -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
@@ -35,7 +37,9 @@ cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
     -DELEMENTS_FLAGS_SET=ON \
     -DM_LIBRARY:STRING="-lm" \
     -DCMAKE_IGNORE_PATH="/opt/local/include;/opt/local/lib;/opt/local/bin" \
+    ${CMAKE_ARGS} \
     ${CMAKE_PLATFORM_FLAGS[@]} \
+    -Wno-dev \
     "${SRC_DIR}"
 
 # Do not use all cores, or the memory consumption will explode!
@@ -43,3 +47,4 @@ make ${MAKEFLAGS}
 make install
 
 install -p "${RECIPE_DIR}/sourcextractor_launch.sh" "${PREFIX}/bin"
+
